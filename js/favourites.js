b50351a7a509227ec92ffb685165c67e801cda37
EliseAur <94222653+EliseAur@users.noreply.github.com>
2022-09-03 16:17:39 +0200

    copy movie

--- js/favourites.js
+++ js/favourites.js
@@ -1,16 +1,17 @@
-import { getExistingFavs } from "./utils/favFunctions.js";
+//FASIT
+// import { getExistingFavs } from "./utils/favFunctions.js";
 
-const favourites = getExistingFavs();
+// const favourites = getExistingFavs();
 
-const productContainer = document.querySelector(".product-container");
+// const productContainer = document.querySelector(".product-container");
 
-if (favourites.length === 0) {
-    productContainer.innerHTML = "No favourites yet";
-}
+// if (favourites.length === 0) {
+//     productContainer.innerHTML = "No favourites yet";
+// }
 
-favourites.forEach((favourite) => {
-    productContainer.innerHTML += `<div class="product">
-                                    <h4>${favourite.name}</h4>
-                                    <i class="fa fa-heart"></i>
-                                </div>`;
-});
+// favourites.forEach((favourite) => {
+//     productContainer.innerHTML += `<div class="product">
+//                                     <h4>${favourite.name}</h4>
+//                                     <i class="fa fa-heart"></i>
+//                                 </div>`;
+// });
--- js/utils/favFunctions.js
+++ js/utils/favFunctions.js
@@ -1,9 +1,10 @@
-export function getExistingFavs() {
-    const favs = localStorage.getItem("favourites");
+//FASIT
+// export function getExistingFavs() {
+//     const favs = localStorage.getItem("favourites");
 
-    if (favs === null) {
-        return [];
-    } else {
-        return JSON.parse(favs);
-    }
-}
+//     if (favs === null) {
+//         return [];
+//     } else {
+//         return JSON.parse(favs);
+//     }
+// }
